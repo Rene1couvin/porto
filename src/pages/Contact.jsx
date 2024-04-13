5b0f273138f92fbb27d0573ed158821789a83db3
Rene1couvin <renefrancisco808@gmal.com>
2024-04-13 12:16:38 +0200

    contact update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -33,21 +33,21 @@ const Contact = () => {
       <form onSubmit={handleSubmit} className="pb-16">
         <div className="mb-4">
           <label htmlFor="name" className="block text-sm font-medium text-gray-500">Name</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="mt-1 block w-1/2 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" required />
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="mt-1 block w-1/2 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 text-black" required />
         </div>
         <div className="mb-4">
           <label htmlFor="email" className="block text-sm font-medium text-gray-500">Email</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 block w-1/2 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" required />
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 block w-1/2 text-black rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" required />
         </div>
         <div className="mb-4">
           <label htmlFor="subject" className="block text-sm font-medium text-gray-500">Subject</label>
-          <input type="text" id="subject" name="subject" value={formData.subject} onChange={handleChange} className="mt-1 block w-1/2 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" required />
+          <input type="text" id="subject" name="subject" value={formData.subject} onChange={handleChange} className="mt-1 text-black block w-1/2 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" required />
         </div>
         <div className="mb-4">
           <label htmlFor="message" className="block text-sm font-medium text-gray-500">Message</label>
-          <textarea id="message" name="message" rows="4" value={formData.message} onChange={handleChange} className="mt-1 block w-1/2 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" required />
+          <textarea id="message" name="message" rows="4" value={formData.message} onChange={handleChange} className="mt-1 block w-1/2 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 text-black" required />
         </div>
-        <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50">Submit</button>
+        <button type="submit" className="bg-yellow-700 text-white py-2 px-4 rounded-md hover:bg-yellow-600 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50">Submit</button>
       </form>
     </div>
   );
